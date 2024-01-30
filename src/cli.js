231e1c82476cb0085fb59ca01c8ba9f7c7a09239
demeena <demeena.msk@gmail.com>
2024-01-30 16:53:12 +0300

    Исправлены конфликты слияния

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,9 @@
-// src/cli.js
+import readlineSync from 'readline-sync';
 
-import readlineSync from 'readline-sync'; // Импортируем библиотеку
-
-// Функция для приветствия пользователя
 const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export default greetUser; // Экспортируем функцию для использования в других файлах
+export default greetUser;
