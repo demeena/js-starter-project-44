61757512767a589754713be9403b5912f98cde36
demeena <demeena.msk@gmail.com>
2024-01-30 22:29:05 +0300

    Add brain-even game logic and update export

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,40 +1,4 @@
 #!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-
-const greetUser = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
-
-const isEven = (number) => number % 2 === 0;
-const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
-
-const playGame = () => {
-  const name = greetUser();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const roundsCount = 3;
-
-  for (let round = 0; round < roundsCount; round += 1) {
-    const question = Math.floor(Math.random() * 100);
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-
-    const correctAnswer = getCorrectAnswer(question);
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-
-    console.log('Correct!');
-  }
-
-  console.log(`Congratulations, ${name}!`);
-};
+import playGame from '../src/games/even.js';
 
 playGame();
---
+++ src/games/even.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (number) => number % 2 === 0;
+
+const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
+const playGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const roundsCount = 3;
+  for (let round = 0; round < roundsCount; round += 1) {
+    const question = Math.floor(Math.random() * 100);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    const correctAnswer = getCorrectAnswer(question);
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+export default playGame;
