4500ad2614d885fa8c489dda4b5608b6ca8d74a9
demeena <demeena.msk@gmail.com>
2024-02-05 14:06:06 +0300

    Исправлена ошибка с импортом в файле brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import playGame from '../src/index.js';
-import getQuestionAndAnswer from '../src/games/prime.js';
+import getQuestionAndAnswer from '../src/games/progression.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
